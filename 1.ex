0b40a52b159459ff6f8f227734af249542a143bc
Diego <diego.luidiblu@gmail.com>
2020-08-24 01:35:30 -0300

    Tipos de Dados

---
+++ 1.ex
@@ -0,0 +1,11 @@
+# Tipos de Dados
+
+1                                   # integer
+0x1F                                # integer
+1.0                                 # float
+true                                # boolean
+:atom                               # atom / symbol
+"elixir"                            # string
+[1, 2, 3]                           # list
+{1, 2, 3}                           # tuple
+%{:a => 1, :b => 2, 3 => false}     # map
