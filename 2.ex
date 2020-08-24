fe29ff5e4e64bd72e09c61b7aebaa8776dfdaae2
Diego <diego.luidiblu@gmail.com>
2020-08-24 01:38:11 -0300

    Tipos de Chamadas

---
+++ 2.ex
@@ -0,0 +1,14 @@
+# Operador em Pipe
+# Variáveis Indermediárias
+
+t1 = argument("arg")
+t2 = ipsum(t1)
+t3 = bar(t2)
+result = foo(t3)
+
+
+# Chamadas dentro de Chamadas
+
+foo(bar(ipsum(argument("arg"))))
+
+# Operador "Pipe"
