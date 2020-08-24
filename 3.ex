53b41f29e1f79bffd0cc099a6fa5abfa04711ae2
Diego <diego.luidiblu@gmail.com>
2020-08-24 01:41:30 -0300

    Operadores

---
+++ 3.ex
@@ -0,0 +1,18 @@
+# Operadores aritiméticos básicos
+
+2.0 = 2 + 5 - 1 * 10 / 2
+
+# Operadores de Listras
+
+[1, 2, 3, 4, 5] = [1, 2, 3] ++ [4, 5, 6] -- [6]
+
+# Concatenação de Strings
+"elixir" = "eli" <> "xir"
+
+# Interpolação de Srings
+nome = "Diego"
+frase = "Olá!, meu nome é #{nome}" 
+
+# Ordem
+1 < 2
+3 > 2
